a42cdf5078a987bf9a8deb5d1186ec07a0cd3cb5
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-02 06:17:49 +0300

    modernize ui ^-^

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -9,7 +9,7 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        "flex h-10 items-center rounded-lg bg-neutral-900 px-4 text-sm font-medium text-white transition-colors hover:bg-neutral-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-900 active:bg-neutral-900 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        "flex h-10 items-center rounded-2xl bg-neutral-900 px-4 text-sm font-medium text-white transition-colors hover:bg-neutral-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-900 active:bg-neutral-900 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
         className
       )}
     >
