5227d7425702b2d8893529e78ce14d3346b2691d
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-05 21:18:49 +0300

    add suspense and skeleton for single component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,20 +7,17 @@ import {
   fetchLatestInvoices,
   fetchRevenue,
 } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const [revenue, latestInvoices, cardData] = await Promise.all([
-    fetchRevenue(),
-    fetchLatestInvoices(),
-    fetchCardData(),
-  ]);
-
+  const latestInvoices = await fetchLatestInvoices();
   const {
-    totalPaidInvoices,
-    totalPendingInvoices,
     numberOfInvoices,
     numberOfCustomers,
-  } = cardData;
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
 
   return (
     <main>
@@ -38,7 +35,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
