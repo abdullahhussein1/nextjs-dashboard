50985b4955f0f55a6a5393c758a957b86b725f87
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-05 20:55:00 +0300

    chapter 8

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Major_Mono_Display } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const poppins = Major_Mono_Display({
+export const lusitana = Major_Mono_Display({
   subsets: ["latin"],
   weight: "400",
 });
--- app/ui/moon-logo.tsx
+++ app/ui/moon-logo.tsx
@@ -1,10 +1,10 @@
 import { MoonIcon } from "@heroicons/react/24/outline";
-import { poppins } from "@/app/ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function MoonLogo() {
   return (
     <div
-      className={`${poppins.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <MoonIcon className="size-10 mr-2 rotate-[15deg]" />
       <p className="text-[44px]">Moon</p>
