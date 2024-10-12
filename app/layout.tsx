1852cb2db92ca4b7db041e7a101642433c6d5599
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-12 22:27:15 +0300

    Alhamdulillah: final step, adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Moon Dashboard",
+    default: "Moon Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, build with App Router.",
+  metadataBase: new URL("https://nextjs-ranya.vercel.app"),
+};
 
 export default function RootLayout({
   children,
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
 import MoonLogo from "@/app/ui/moon-logo";
 import LoginForm from "@/app/ui/login-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export default function LoginPage() {
   return (
