ac43096ce536e56b87660cb96eb3893072de83b1
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-12 21:53:32 +0300

    adding authentication

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
-import AcmeLogo from "@/app/ui/moon-logo";
+import MoonLogo from "@/app/ui/moon-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
+import { signOut } from "@/auth";
 
 export default function SideNav() {
   return (
@@ -10,12 +11,17 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-3xl bg-neutral-900 p-4 md:h-40"
         href="/"
       >
-        <AcmeLogo />
+        <MoonLogo />
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-3xl bg-gray-50 md:block"></div>
-        <form>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-3xl bg-gray-50 p-3 text-sm font-medium hover:bg-neutral-5000/30 hover:text-neutral-900 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
